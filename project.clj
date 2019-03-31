b53c098ea197fee82ae9c39bc6ac9835c4586c66
Torkelv <torkel93@gmail.com>
2019-03-31 18:44:11 +0200

    clj10 upgrade

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "FIXME: write descption"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring-server "0.3.1"]
+                 [ring-server "0.5.0"]
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler itnorge.core/app})
 
